fd3892ffdd0ea3ae6eb85e50bb7163f585fb6ace
sherlockwisdom <wisdomnji@gmail.com>
2026-05-16 23:17:49 +0100

    update: fix test

--- tests/email_test.rs
+++ tests/email_test.rs
@@ -6,11 +6,11 @@ fn test_email_init() {
     let to  = "to";
     let body = "body";
     let subject = "subject";
-    let from = "from";
+    let from_id: u8 = 0x1;
     let email = Email::new(
         to,
         body,
         subject,
-        from,
+        &from_id
     );
 }
