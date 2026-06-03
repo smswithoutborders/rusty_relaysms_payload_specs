dfaf3a409cbc00310b5d81952f0162b1392d2f44
sherlock <wisdomnji@gmail.com>
2026-06-03 22:41:17 +0100

    update: v1 publisher now added

--- src/v1/encrypt.rs
+++ src/v1/encrypt.rs
@@ -1,4 +1,5 @@
 use std::io::Read;
+use std::sync::Arc;
 use aead::{Payload};
 use sha2::Sha256;
 use hkdf::Hkdf;
@@ -28,6 +29,26 @@ fn token_key_derivation(
     dh: SharedSecret,
     dh1: SharedSecret
 ) -> Vec<u8> {
+    const TOKEN_PROTOCOL: &[u8] = b"3DH_25519_AESGCM";
+    const TOKEN_SALT: &[u8] = b"RelaySMS v1";
+    const TOKEN_DS: &[u8] = b"RelaySMS Encryption Key v1";
+    let mut iter = dh.to_bytes().into_iter().chain(dh1.to_bytes());
+    let con_dh_dh1: [u8; 64] = std::array::from_fn(|i| { iter.next().unwrap() });
+    let (_, hk) = Hkdf::<Sha256>::extract(
+        Option::from(TOKEN_SALT), con_dh_dh1.as_slice());
+    let info = [TOKEN_PROTOCOL, [0x00u8].as_slice(), TOKEN_DS].concat();
+    let mut key = [0u8; 32];
+    hk.expand(info.as_slice(), &mut key).expect("expansion should be ok");
+    key.to_vec()
+}
+
+fn publisher_key_derivation(
+    dh: SharedSecret,
+    dh1: SharedSecret
+) -> Vec<u8> {
+    const TOKEN_PROTOCOL: &[u8] = b"3DH_25519_ChaCha20-Poly1305";
+    const TOKEN_SALT: &[u8] = b"RelaySMS v1";
+    const TOKEN_DS: &[u8] = b"RelaySMS Publishing Key v1";
     let mut iter = dh.to_bytes().into_iter().chain(dh1.to_bytes());
     let con_dh_dh1: [u8; 64] = std::array::from_fn(|i| { iter.next().unwrap() });
     let (_, hk) = Hkdf::<Sha256>::extract(
@@ -58,9 +79,6 @@ associated_data = Key-id || eC_kid_pk || eS_kid_pk
 token = AEAD_ENCRYPT(input = request_string, key = aes_key, ad = associated_data, nonce=nonce)
 */
 
-const TOKEN_PROTOCOL: &[u8] = b"3DH_25519_AESGCM";
-const TOKEN_SALT: &[u8] = b"RelaySMS v1";
-const TOKEN_DS: &[u8] = b"RelaySMS Encryption Key v1";
 
 #[uniffi::export]
 fn v1_token_encrypt(
@@ -116,6 +134,33 @@ fn v1_token_encrypt(
     }
 }
 
+fn triple_dh_decryption(
+    key_id: u8,
+    ec_kid_pk: Vec<u8>,
+    es_kid_pk: Vec<u8>,
+    ss_kid: Vec<u8>,
+    es_kid: Vec<u8>,
+) -> Result<(SharedSecret, SharedSecret, Vec<u8>), V1CryptographicError>{
+    let associated_data = [
+        key_id.to_le_bytes().to_vec(),
+        ec_kid_pk.clone(),
+        es_kid_pk.clone()
+    ].concat();
+
+    let ec_kid_pk: [u8; 32] = ec_kid_pk.try_into().expect("ec_kid_pk should be 32 bytes");
+    let ec_kid_pk= PublicKey::from(ec_kid_pk);
+
+    let ss_kid: [u8; 32] = ss_kid.try_into().expect("ss_kid should be 32 bytes");
+    let ss_kid_secret = StaticSecret::from(ss_kid);
+
+    let es_kid: [u8; 32] = es_kid.try_into().expect("es_kid should be 32 bytes");
+    let es_kid_secret = StaticSecret::from(es_kid);
+
+    let dh = ss_kid_secret.diffie_hellman(&ec_kid_pk);
+    let dh1 = es_kid_secret.diffie_hellman(&ec_kid_pk);
+    Ok((dh, dh1, associated_data.to_vec()))
+}
+
 /*
 Returns TokenHash
  */
@@ -129,27 +174,22 @@ fn v1_token_decrypt(
     received_payload: Vec<u8>,
 ) -> Result<Vec<u8>, V1CryptographicError> {
     let nonce = Nonce::try_from([0x00u8; 12]).unwrap();
-    let associated_data = [
-        key_id.to_le_bytes().to_vec(),
-        ec_kid_pk.clone(),
-        es_kid_pk.clone()
-    ].concat();
+    let (dh, dh1, aad) = match triple_dh_decryption(
+        key_id,
+        ec_kid_pk,
+        es_kid_pk,
+        ss_kid,
+        es_kid
+    ) {
+        Ok((dh1, dh2, aad)) => (dh1, dh2, aad),
+        Err(e) => return Err(V1CryptographicError::FailedToDecrypt {
+            err: e.to_string(),
+        })
+    };
     let payload = Payload {
         msg: received_payload.as_slice(),
-        aad: associated_data.as_slice(),
+        aad: aad.as_slice(),
     };
-
-    let ec_kid_pk: [u8; 32] = ec_kid_pk.try_into().expect("ec_kid_pk should be 32 bytes");
-    let ec_kid_pk= PublicKey::from(ec_kid_pk);
-
-    let ss_kid: [u8; 32] = ss_kid.try_into().expect("ss_kid should be 32 bytes");
-    let ss_kid_secret = StaticSecret::from(ss_kid);
-
-    let es_kid: [u8; 32] = es_kid.try_into().expect("es_kid should be 32 bytes");
-    let es_kid_secret = StaticSecret::from(es_kid);
-
-    let dh = ss_kid_secret.diffie_hellman(&ec_kid_pk);
-    let dh1 = es_kid_secret.diffie_hellman(&ec_kid_pk);
 
     let aes_key = token_key_derivation(dh, dh1);
     let cipher = Aes256Gcm::new_from_slice(&aes_key)
@@ -164,6 +204,7 @@ fn v1_token_decrypt(
         })
     }
 }
+
 
 fn parse_for_token_hash(data: Vec<u8>) -> Vec<u8> {
     let method_name_len = data[0];
@@ -191,7 +232,7 @@ nonce = nonce
 )
 */
 #[uniffi::export]
-fn v1_platform_publisher(
+fn v1_platform_publisher_encrypt(
     ec_kid: Vec<u8>,
     ss_kid_pk: Vec<u8>,
     es_kid_pk: Vec<u8>,
@@ -247,6 +288,46 @@ fn v1_platform_publisher(
     }
 }
 
+
+#[uniffi::export]
+fn v1_platform_publisher_decrypt(
+    ec_kid_pk: Vec<u8>,
+    es_kid_pk: Vec<u8>,
+    ss_kid: Vec<u8>,
+    es_kid: Vec<u8>,
+    key_id: u8,
+    received_payload: Vec<u8>,
+) -> Result<Vec<u8>, V1CryptographicError> {
+    let nonce = Nonce::try_from([0x00u8; 12]).unwrap();
+    let (dh, dh1, aad) = match triple_dh_decryption(
+        key_id,
+        ec_kid_pk,
+        es_kid_pk,
+        ss_kid,
+        es_kid
+    ) {
+        Ok((dh1, dh2, aad)) => (dh1, dh2, aad),
+        Err(e) => return Err(V1CryptographicError::FailedToDecrypt {
+            err: e.to_string(),
+        })
+    };
+    let payload = Payload {
+        msg: received_payload.as_slice(),
+        aad: aad.as_slice(),
+    };
+
+    let chacha_key = publisher_key_derivation(dh, dh1);
+    let cipher = ChaCha20Poly1305::new_from_slice(&chacha_key)
+        .expect("ChaCha20Poly1305::new_from_slice failed");
+
+    match cipher.decrypt(&nonce, payload) {
+        Ok(decrypted_payload) => Ok(decrypted_payload),
+        Err(e) => Err(V1CryptographicError::FailedToDecrypt {
+            err: e.to_string(),
+        })
+    }
+}
+
 #[test]
 fn test_token_encryption_decryption() {
     let rng: [u8; 32] = rand::rng().random();
@@ -286,4 +367,43 @@ fn test_token_encryption_decryption() {
     ).unwrap();
 
     assert_eq!(token_hash.to_vec(), decrypted);
+}
+
+#[test]
+fn test_publisher_encryption_decryption() {
+    let rng: [u8; 32] = rand::rng().random();
+    let ec_kid = StaticSecret::from(rng);
+
+    let rng: [u8; 32] = rand::rng().random();
+    let ss_kid = StaticSecret::from(rng);
+    let ss_kid_pk = PublicKey::from(&ss_kid).as_bytes().to_vec();
+
+    let rng: [u8; 32] = rand::rng().random();
+    let es_kid = StaticSecret::from(rng);
+    let es_kid_pk = PublicKey::from(&es_kid).as_bytes().to_vec();
+
+    let plaintext= b"129701738923479234023840384h3hsfhasdf7123947293472394732497";
+    let key_id= 255u8;
+    let ciphertext = v1_platform_publisher_encrypt(
+        ec_kid.as_bytes().to_vec(),
+        ss_kid_pk,
+        es_kid_pk,
+        key_id,
+        plaintext.to_vec(),
+    ).unwrap();
+
+    let ec_kid_pk = PublicKey::from(&ec_kid).as_bytes().to_vec();
+    let es_kid_pk = PublicKey::from(&es_kid).as_bytes().to_vec();
+    let ss_kid = ss_kid.as_bytes().to_vec();
+    let es_kid = es_kid.as_bytes().to_vec();
+    let decrypted = v1_platform_publisher_decrypt(
+        ec_kid_pk,
+        es_kid_pk,
+        ss_kid,
+        es_kid,
+        key_id,
+        ciphertext,
+    ).unwrap();
+
+    assert_eq!(plaintext.to_vec(), decrypted);
 }
