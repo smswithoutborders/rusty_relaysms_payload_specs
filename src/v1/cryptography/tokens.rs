25ef181798c5d3a8f2a56aa453594a25d480776b
sherlockwisdom <wisdomnji@gmail.com>
2026-06-12 14:18:24 +0100

    update: moved methods for client encrypt and decrypt with server too

--- src/v1/cryptography/tokens.rs
+++ src/v1/cryptography/tokens.rs
@@ -26,44 +26,6 @@ fn token_key_derivation(
 /*
 Returns TokenHash
  */
-#[uniffi::export]
-fn v1_token_decrypt(
-    ec_kid_pk: Vec<u8>,
-    es_kid_pk: Vec<u8>,
-    ss_kid: Vec<u8>,
-    es_kid: Vec<u8>,
-    key_id: u8,
-    received_payload: Vec<u8>,
-) -> Result<Vec<u8>, V1CryptographicError> {
-    let nonce = Nonce::try_from([0x00u8; 12]).unwrap();
-    let (dh, dh1, aad) = match triple_dh_decryption(
-        key_id,
-        ec_kid_pk,
-        es_kid_pk,
-        ss_kid,
-        es_kid
-    ) {
-        Ok((dh1, dh2, aad)) => (dh1, dh2, aad),
-        Err(e) => return Err(V1CryptographicError::FailedToDecrypt {
-            err: e.to_string(),
-        })
-    };
-    let payload = Payload {
-        msg: received_payload.as_slice(),
-        aad: aad.as_slice(),
-    };
-
-    let aes_key = token_key_derivation(dh, dh1);
-    let cipher = Aes256Gcm::new_from_slice(&aes_key)
-        .expect("Aes256Gcm::new_from_slice failed");
-
-    match cipher.decrypt(&nonce, payload) {
-        Ok(decrypted_payload) => Ok(decrypted_payload),
-        Err(e) => Err(V1CryptographicError::FailedToDecrypt {
-            err: e.to_string(),
-        })
-    }
-}
 
 /*
 protocol = “3DH_25519_AESGCM”
@@ -78,7 +40,7 @@ associated_data = Key-id || eC_kid_pk || eS_kid_pk
 encrypted_url = AEAD_ENCRYPT(input = Token, key = aes_key, ad = associated_data, nonce=nonce)
  */
 #[uniffi::export]
-fn v1_token_encrypt(
+fn v1_token_encrypt_client(
     ec_kid: Vec<u8>,
     ss_kid_pk: Vec<u8>,
     es_kid_pk: Vec<u8>,
@@ -93,7 +55,8 @@ fn v1_token_encrypt(
     let associated_data = [
         key_id.to_le_bytes().to_vec(),
         ec_kid_pk.to_bytes().to_vec(),
-        es_kid_pk.clone()
+        es_kid_pk.to_vec(),
+        ss_kid_pk.to_vec(),
     ].concat();
 
     let payload = Payload {
@@ -121,10 +84,157 @@ fn v1_token_encrypt(
     }
 }
 
+
+#[uniffi::export]
+fn v1_token_decrypt_server(
+    ss_kid: Vec<u8>,
+    es_kid: Vec<u8>,
+    ec_kid_pk: Vec<u8>,
+    key_id: u8,
+    ciphertext: Vec<u8>,
+) -> Result<Vec<u8>, V1CryptographicError> {
+    let nonce = Nonce::try_from([0x00u8; 12]).unwrap();
+
+    let ss_kid: [u8; 32] = ss_kid.try_into().expect("ss_kid should be 32 bytes");
+    let ss_kid = StaticSecret::from(ss_kid);
+
+    let es_kid: [u8; 32] = es_kid.try_into().expect("ss_kid should be 32 bytes");
+    let es_kid = StaticSecret::from(es_kid);
+
+    let ec_kid_pk: [u8; 32] = ec_kid_pk.try_into().expect("ec_kid_pk should be 32 bytes");
+    let ec_kid_pk = PublicKey::from(ec_kid_pk);
+
+    let dh = ss_kid.diffie_hellman(&ec_kid_pk);
+    let dh1 = es_kid.diffie_hellman(&ec_kid_pk);
+
+    let es_kid_pk= PublicKey::from(&es_kid);
+    let ss_kid_pk= PublicKey::from(&ss_kid);
+    let associated_data = [
+        key_id.to_le_bytes().to_vec(),
+        ec_kid_pk.to_bytes().to_vec(),
+        es_kid_pk.to_bytes().to_vec(),
+        ss_kid_pk.to_bytes().to_vec(),
+    ].concat();
+
+    let payload = Payload {
+        msg: ciphertext.as_slice(),
+        aad: associated_data.as_slice(),
+    };
+
+    let aes_key = token_key_derivation(dh, dh1);
+    let cipher = Aes256Gcm::new_from_slice(&aes_key)
+        .expect("Aes256Gcm::new_from_slice failed");
+
+    match cipher.decrypt(&nonce, payload) {
+        Ok(decrypted_payload) => Ok(decrypted_payload),
+        Err(e) => Err(V1CryptographicError::FailedToDecrypt {
+            err: e.to_string(),
+        })
+    }
+}
+
+#[uniffi::export]
+fn v1_token_encrypt_server(
+    ss_kid: Vec<u8>,
+    es_kid: Vec<u8>,
+    ec_kid_pk: Vec<u8>,
+    key_id: u8,
+    token: &[u8],
+) -> Result<Vec<u8>, V1CryptographicError> {
+    let nonce = Nonce::try_from([0x00u8; 12]).unwrap();
+
+    let ec_kid_pk: [u8; 32] = ec_kid_pk.try_into().expect("ec_kid_pk should be 32 bytes");
+    let ec_kid_pk= PublicKey::from(ec_kid_pk);
+
+    let ss_kid: [u8; 32] = ss_kid.try_into().expect("ss_kid should be 32 bytes");
+    let ss_kid = StaticSecret::from(ss_kid);
+
+    let es_kid: [u8; 32] = es_kid.try_into().expect("es_kid should be 32 bytes");
+    let es_kid = StaticSecret::from(es_kid);
+
+    let dh = ss_kid.diffie_hellman(&ec_kid_pk);
+    let dh1 = es_kid.diffie_hellman(&ec_kid_pk);
+
+    let es_kid_pk= PublicKey::from(&es_kid);
+    let ss_kid_pk = PublicKey::from(&ss_kid);
+
+    let associated_data = [
+        key_id.to_le_bytes().to_vec(),
+        ec_kid_pk.to_bytes().to_vec(),
+        es_kid_pk.to_bytes().to_vec(),
+        ss_kid_pk.to_bytes().to_vec(),
+    ].concat();
+
+    let payload = Payload {
+        msg: token,
+        aad: associated_data.as_slice(),
+    };
+
+    let aes_key = token_key_derivation(dh, dh1);
+    let cipher = Aes256Gcm::new_from_slice(&aes_key)
+        .expect("Aes256Gcm::new_from_slice failed");
+    match cipher.encrypt(&nonce, payload) {
+        Ok(ciphertext) => Ok(ciphertext.to_vec()),
+        Err(e) => Err(V1CryptographicError::FailedToEncrypt {
+            err: e.to_string(),
+        })
+    }
+}
+
+
+#[uniffi::export]
+fn v1_token_decrypt_client(
+    ec_kid: Vec<u8>,
+    ss_kid_pk: Vec<u8>,
+    es_kid_pk: Vec<u8>,
+    key_id: u8,
+    received_payload: Vec<u8>,
+) -> Result<Vec<u8>, V1CryptographicError> {
+    let nonce = Nonce::try_from([0x00u8; 12]).unwrap();
+
+    let ec_kid: [u8; 32] = ec_kid.try_into().expect("ec_kid should be 32 bytes");
+    let ec_kid = StaticSecret::from(ec_kid);
+
+    let ss_kid_pk: [u8; 32] = ss_kid_pk.try_into().expect("ss_kid_pk should be 32 bytes");
+    let ss_kid_pk = PublicKey::from(ss_kid_pk);
+
+    let es_kid_pk: [u8; 32] = es_kid_pk.try_into().expect("es_kid_pk should be 32 bytes");
+    let es_kid_pk = PublicKey::from(es_kid_pk);
+
+    let dh = ec_kid.diffie_hellman(&ss_kid_pk);
+    let dh1 = ec_kid.diffie_hellman(&es_kid_pk);
+
+    let ec_kid_pk= PublicKey::from(&ec_kid);
+    let associated_data = [
+        key_id.to_le_bytes().to_vec(),
+        ec_kid_pk.to_bytes().to_vec(),
+        es_kid_pk.to_bytes().to_vec(),
+        ss_kid_pk.to_bytes().to_vec(),
+    ].concat();
+
+
+    let payload = Payload {
+        msg: received_payload.as_slice(),
+        aad: associated_data.as_slice(),
+    };
+
+    let aes_key = token_key_derivation(dh, dh1);
+    let cipher = Aes256Gcm::new_from_slice(&aes_key)
+        .expect("Aes256Gcm::new_from_slice failed");
+
+    match cipher.decrypt(&nonce, payload) {
+        Ok(decrypted_payload) => Ok(decrypted_payload),
+        Err(e) => Err(V1CryptographicError::FailedToDecrypt {
+            err: e.to_string(),
+        })
+    }
+}
+
 #[test]
 fn test_token_encryption_decryption() {
     let rng: [u8; 32] = rand::rng().random();
     let ec_kid = StaticSecret::from(rng);
+    let ec_kid_pk = PublicKey::from(&ec_kid).as_bytes().to_vec();
 
     let rng: [u8; 32] = rand::rng().random();
     let ss_kid = StaticSecret::from(rng);
@@ -136,23 +246,38 @@ fn test_token_encryption_decryption() {
 
     let token_hash= b"Sample token hash";
     let key_id= 255u8;
-    let ciphertext = v1_token_encrypt(
-        ec_kid.as_bytes().to_vec(),
+
+    let ciphertext = v1_token_encrypt_server(
+        ss_kid.to_bytes().to_vec(),
+        es_kid.to_bytes().to_vec(),
+        ec_kid_pk.clone(),
+        key_id,
+        token_hash
+    ).unwrap();
+
+    let decrypted = v1_token_decrypt_client(
+        ec_kid.to_bytes().to_vec(),
+        ss_kid_pk.clone(),
+        es_kid_pk.clone(),
+        key_id,
+        ciphertext,
+    ).unwrap();
+
+    assert_eq!(token_hash.to_vec(), decrypted);
+
+
+    let ciphertext = v1_token_encrypt_client(
+        ec_kid.to_bytes().to_vec(),
         ss_kid_pk,
         es_kid_pk,
         key_id,
         token_hash,
     ).unwrap();
 
-    let ec_kid_pk = PublicKey::from(&ec_kid).as_bytes().to_vec();
-    let es_kid_pk = PublicKey::from(&es_kid).as_bytes().to_vec();
-    let ss_kid = ss_kid.as_bytes().to_vec();
-    let es_kid = es_kid.as_bytes().to_vec();
-    let decrypted = v1_token_decrypt(
+    let decrypted = v1_token_decrypt_server(
+        ss_kid.to_bytes().to_vec(),
+        es_kid.to_bytes().to_vec(),
         ec_kid_pk,
-        es_kid_pk,
-        ss_kid,
-        es_kid,
         key_id,
         ciphertext,
     ).unwrap();
